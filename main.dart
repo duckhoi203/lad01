19a7f675049ac86cc41231602adfd51bae0da648
duckhoi203 <khoituko@gmail.com>
2023-12-13 22:24:12 +0700

    lad01

---
+++ main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const Center(
+      child: Text(
+        'Nguyễn Đức KHôi ,Hello, World!',
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+}
